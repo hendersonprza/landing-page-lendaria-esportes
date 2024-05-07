dc244f3cdbf2375d422fbe2b48aade91604b7641
hendersonprza <hendersoneev@gmail.com>
2024-05-07 07:28:59 -0300

    Landing Page Finalizada

---
+++ script.js
@@ -0,0 +1,11 @@
+let showMenu = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+    menuContent.classList.toggle('on', showMenu);
+
+    showMenu = !showMenu;
+});
+
+
